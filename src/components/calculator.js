36fe0f1335e6b97033aa5efaea2e3a1cb0c41c33
felixDev22 <felixnzomo@gmail.com>
2023-01-10 21:37:17 +0300

    Move row , btn to cal css and Add btn-2 class

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -23,30 +23,30 @@ export default class Calculator extends React.Component {
               <button className='btn'>AC</button>
               <button className='btn'>+/-</button>
               <button className='btn'>%</button>
-              <button className='btn'>&#247</button>
+              <button className='btn-2'>&#247</button>
             </li>
             <li className='row'>
               <button className='btn'>7</button>
               <button className='btn'>8</button>
               <button className='btn'>9</button>
-              <button className='btn'>X</button>
+              <button className='btn-2'>X</button>
             </li>
             <li className='row'>
               <button className='btn'>4</button>
               <button className='btn'>5</button>
               <button className='btn'>6</button>
-              <button className='btn'>-</button>
+              <button className='btn-2'>-</button>
             </li>
             <li className='row'>
               <button className='btn'>3</button>
               <button className='btn'>2</button>
               <button className='btn'>1</button>
-              <button className='btn'>+</button>
+              <button className='btn-2'>+</button>
             </li>
             <li className='row2'>
               <button className='btn'>0</button>
               <button className='btn'>.</button>
-              <button className='btn'>=</button>
+              <button className='btn-2'>=</button>
             </li>
           </ul>
         </div>
