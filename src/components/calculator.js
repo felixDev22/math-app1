b41bb19d2a61ccecd6b3a64da8b055cef209ba63
felixDev22 <felixnzomo@gmail.com>
2023-01-11 11:48:19 +0300

    test

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,91 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './Calculator.css';
 
-export default class Calculator extends Component {
+export default class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
 
   render() {
-    return (
-      <div className="cal">
-        <div className="displayArea">
-          <input className="cal-input" type="text" value="0" readOnly />
-        </div>
-        <div className="operators">
-          <ul className="rows">
-            <li className="row">
-              <button type="button" className="btn">
-                AC
-              </button>
-              <button type="button" className="btn">
-                +/-
-              </button>
-              <button type="button" className="btn">
-                %
-              </button>
-              <button type="button" className="btn-2">
-                &#247;
-              </button>
-            </li>
-            <li className="row">
-              <button type="button" className="btn">
-                7
-              </button>
-              <button type="button" className="btn">
-                8
-              </button>
-              <button type="button" className="btn">
-                9
-              </button>
-              <button type="button" className="btn-2">
-                x
-              </button>
-            </li>
-            <li className="row">
-              <button type="button" className="btn">
-                4
-              </button>
-              <button type="button" className="btn">
-                5
-              </button>
-              <button type="button" className="btn">
-                6
-              </button>
-              <button type="button" className="btn-2">
-                -
-              </button>
-            </li>
-            <li className="row">
-              <button type="button" className="btn">
-                1
-              </button>
-              <button type="button" className="btn">
-                2
-              </button>
-              <button type="button" className="btn">
-                3
-              </button>
-              <button type="button" className="btn-2">
-                +
-              </button>
-            </li>
-            <li className="row2">
-              <button type="button" className="btn">
-                0
-              </button>
-              <button type="button" className="btn">
-                .
-              </button>
-              <button type="button" className="btn-2">
-                =
-              </button>
-            </li>
-          </ul>
-        </div>
-      </div>
-    );
+    return <h1>hallo world</h1>;
   }
 }
