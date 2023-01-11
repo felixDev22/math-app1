ae48378e06adc15d6a925a5dcdc0ed165eff21b8
felixDev22 <felixnzomo@gmail.com>
2023-01-11 23:54:38 +0300

    Add onClick on the buttons

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -9,78 +9,98 @@ class CalculatorUI extends React.Component {
     this.state = {};
   }
 
-  render() {
+  render() { 
     return (
       <div className="cal">
         <div className="displayArea">
-          <input className="cal-input" type="text" value="0" readOnly />
+          <p className="cal-input" type="text">{result || 0}</p>
         </div>
         <div className="operators">
           <ul className="rows">
             <li className="row">
-              <button type="button" className="btn">
+              <div type="button" className="btn" onClick=onClick={this.onClickEventHandler}>
                 AC
-              </button>
-              <button type="button" className="btn">
+              </div>
+              <button type="button" className="btn" onClick=onClick={this.onClickEventHandler}>
                 +/-
               </button>
-              <button type="button" className="btn">
+              <button type="button" className="btn" onClick=onClick={this.onClickEventHandler}>
                 %
               </button>
-              <button type="button" className="btn-2">
+              <button
+                type="button"
+                className="btn-2"
+                onClick=onClick={this.onClickEventHandler}
+              >
                 &#247;
               </button>
             </li>
             <li className="row">
-              <button type="button" className="btn">
+              <button type="button" className="btn" onClick=onClick={this.onClickEventHandler}>
                 7
               </button>
-              <button type="button" className="btn">
+              <button type="button" className="btn" onClick=onClick={this.onClickEventHandler}>
                 8
               </button>
-              <button type="button" className="btn">
+              <button type="button" className="btn" onClick=onClick={this.onClickEventHandler}>
                 9
               </button>
-              <button type="button" className="btn-2">
+              <button
+                type="button"
+                className="btn-2"
+                onClick=onClick={this.onClickEventHandler}
+              >
                 x
               </button>
             </li>
             <li className="row">
-              <button type="button" className="btn">
+              <button type="button" className="btn" onClick=onClick={this.onClickEventHandler}>
                 4
               </button>
-              <button type="button" className="btn">
+              <button type="button" className="btn" onClick=onClick={this.onClickEventHandler}>
                 5
               </button>
-              <button type="button" className="btn">
+              <button type="button" className="btn" onClick=onClick={this.onClickEventHandler}>
                 6
               </button>
-              <button type="button" className="btn-2">
+              <button
+                type="button"
+                className="btn-2"
+                onClick=onClick={this.onClickEventHandler}
+              >
                 -
               </button>
             </li>
             <li className="row">
-              <button type="button" className="btn">
+              <button type="button" className="btn" onClick=onClick={this.onClickEventHandler}>
                 1
               </button>
-              <button type="button" className="btn">
+              <button type="button" className="btn" onClick=onClick={this.onClickEventHandler}>
                 2
               </button>
-              <button type="button" className="btn">
+              <button type="button" className="btn" onClick=onClick={this.onClickEventHandler}>
                 3
               </button>
-              <button type="button" className="btn-2">
+              <button
+                type="button"
+                className="btn-2"
+                onClick=onClick={this.onClickEventHandler}
+              >
                 +
               </button>
             </li>
             <li className="row2">
-              <button type="button" className="btn">
+              <button type="button" className="btn" onClick=onClick={this.onClickEventHandler}>
                 0
               </button>
-              <button type="button" className="btn">
+              <button type="button" className="btn" onClick=onClick={this.onClickEventHandler}>
                 .
               </button>
-              <button type="button" className="btn-2">
+              <button
+                type="button"
+                className="btn-2"
+                onClick=onClick={this.onClickEventHandler}
+              >
                 =
               </button>
             </li>
