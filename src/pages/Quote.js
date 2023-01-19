63c5a00756d788ed85d87bc725d902d85813ca1c
felixDev22 <felixnzomo@gmail.com>
2023-01-19 15:59:34 +0300

    Add quote, add quote.modules.css file, update app.css

---
+++ src/pages/Quote.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import classes from "./Quote.module.css";
+
+const Quote = () => (
+  <section className={classes.quote_section}>
+    <h2>Quote</h2>
+    <p>
+      It is not knowledge, but the act of learning, not possession but the act
+      of getting there, which grants the greatest enjoyment.
+    </p>
+  </section>
+);
+
+export default Quote;
