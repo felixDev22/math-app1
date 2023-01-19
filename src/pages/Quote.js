4df484aa2ecce3adac006297ea14a5dc9d0a79f7
felixDev22 <felixnzomo@gmail.com>
2023-01-19 18:49:39 +0300

    Add quote icon, add author

--- src/pages/Quote.js
+++ src/pages/Quote.js
@@ -1,13 +1,17 @@
 import React from 'react';
-
 import classes from './Quote.module.css';
 
 const Quote = () => (
   <section className={classes.quote_section}>
     <h2>Quote</h2>
+    <span>
+      <i className="fa-sharp fa-solid fa-quote-left" />
+    </span>
     <p>
       It is not knowledge, but the act of learning, not possession but the act
       of getting there, which grants the greatest enjoyment.
+      <br />
+      <b>Carl Friedrich Gauss.</b>
     </p>
   </section>
 );
