be524a092b576c2fb310d5e68c1520d17e83914e
felixDev22 <felixnzomo@gmail.com>
2023-01-21 17:42:31 +0300

    Add more user test interaction

--- src/__test__/calculator.test.js
+++ src/__test__/calculator.test.js
@@ -10,17 +10,61 @@ describe('testing calculator component with user interaction', () => {
     expect(tree).toMatchSnapshot();
   });
 
-  test('Should calculate the value', () => {
+  test('Addition Result should be 6', () => {
     render(<CalculatorUI />);
-    const btn1 = screen.getByRole('button', { name: /1/i });
+    const btn1 = screen.getByRole('button', { name: '1' });
     userEvent.click(btn1);
-    const btnOperation = screen.getByRole('button', { name: /+/i });
+    const btnOperation = screen.getByRole('button', { name: '+' });
     userEvent.click(btnOperation);
-    const btn2 = screen.getByRole('button', { name: /5/i });
+    const btn2 = screen.getByRole('button', { name: '5' });
     userEvent.click(btn2);
-    const btnEqualto = screen.getByRole('button', { name: /=/i });
+    const btnEqualto = screen.getByRole('button', { name: '=' });
     userEvent.click(btnEqualto);
     const result = screen.getByTestId('total_value');
-    expect(result.testContent).toBe('6');
+    expect(result.textContent).toBe('6');
+  });
+
+  test('Substraction result should be 4', () => {
+    render(<CalculatorUI />);
+    const btn1 = screen.getByRole('button', { name: '9' });
+    userEvent.click(btn1);
+    const btnOperation = screen.getByRole('button', { name: '-' });
+    userEvent.click(btnOperation);
+    const btn2 = screen.getByRole('button', { name: '5' });
+    userEvent.click(btn2);
+    const btnEqualto = screen.getByRole('button', { name: '=' });
+    userEvent.click(btnEqualto);
+    const result = screen.getByTestId('total_value');
+    expect(result.textContent).toBe('4');
+  });
+
+  test('Multiplation result should be 16', () => {
+    render(<CalculatorUI />);
+    const btn1 = screen.getByRole('button', { name: '4' });
+    userEvent.click(btn1);
+    const btnOperation = screen.getByRole('button', { name: /x/i });
+    userEvent.click(btnOperation);
+    const btn2 = screen.getByRole('button', { name: '4' });
+    userEvent.click(btn2);
+    const btnEqualto = screen.getByRole('button', { name: '=' });
+    userEvent.click(btnEqualto);
+    const result = screen.getByTestId('total_value');
+    expect(result.textContent).toBe('16');
+  });
+
+  test('Division result should be 2', () => {
+    render(<CalculatorUI />);
+    const btn1 = screen.getByRole('button', { name: '2' });
+    userEvent.click(btn1);
+    userEvent.click(btn1);
+    const btnOperation = screen.getByRole('button', { name: /÷/i });
+    userEvent.click(btnOperation);
+    const btn2 = screen.getByRole('button', { name: '1' });
+    userEvent.click(btn2);
+    userEvent.click(btn2);
+    const btnEqualto = screen.getByRole('button', { name: '=' });
+    userEvent.click(btnEqualto);
+    const result = screen.getByTestId('total_value');
+    expect(result.textContent).toBe('2');
   });
 });
